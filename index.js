434fb73fde2e1d30c01d5c53a88bf5f8c7be8ae6
Jhon Wilmer Azcarate Rivera <unavidahonorable@hotmail.com>
2022-06-20 15:13:14 -0500

    Se hicieron modificaciones que permiten al usuario ingresar su peso y ver el resultado  en la pagina , cambios vistos en la clase 8

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ var g_jupiter = 24.8;
 
 //variables para saber el peso del usuario
 
+/*en esta variable se ingresa un dato especificado por el usuario usando un promt(), 
+cabe recalcar que el dato ingresado por el usuario es un string*/ 
+var entrada_usuario=prompt("Ingresa tu peso: ");
+
 /*en el video no inicializan la variable, pero para especificar que sera un numero 
 inicializo la variable con 0*/
 var peso_final = 0;
-/*El peso del usuario sera ejemplificado con la variable (peso_usuario) que 
-para esta version seran 77 kilogramos*/
-var peso_usuario = 77;
+/*Un cambio visto en la clase 8 es añadir el peso usando un promnt (mirar linea 13), y a continuacion
+la variable peso_usuario transforma la entrada que es un string a un numero 
+de punto flotante o (float) usando la funcion parseFloat()*/
+var peso_usuario = parseFloat(entrada_usuario) ;
 
 //operacion matematica que dira el precio del usuario en marte
 peso_final = peso_usuario * g_marte / g_tierra;
@@ -22,4 +27,4 @@ peso_final = peso_usuario * g_marte / g_tierra;
 peso_final = parseInt(peso_final);
 
 //Retorno en el document: esta linea escribe algo en el documento html donde se encuentra vinculado
-document.write("tu peso en marte es: <b>"+peso_final+" kg </b>")
+document.write("tu peso en marte es: <b>"+peso_final+" kg </b>");
