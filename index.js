9125f4cff2ad79df15e4f718010d2357b08234a7
Jhon Wilmer Azcarate Rivera <unavidahonorable@hotmail.com>
2022-06-20 14:32:33 -0500

    ESTA ES LA VERSION DEL PROYECTO BASADO EN EL QUE APARECE EN LA CLASE 7 DEL CURSO DE PROGRAMACION BASICA DE PLATZI

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-var usuario=prompt("por favor digite su peso");
-document.write("tu peso es "+usuario);
+/*Esta version es solo un ejemplo visto de la clase 7 a 9 del curso de programacion basica de platzi,
+se modificara en dias futuros*/
+
+//variables que indican la gravedad en cada planeta
+var g_tierra = 9.8;
+var g_marte = 3.7;
+var g_jupiter = 24.8;
+
+//variables para saber el peso del usuario
+
+/*en el video no inicializan la variable, pero para especificar que sera un numero 
+inicializo la variable con 0*/
+var peso_final = 0;
+/*El peso del usuario sera ejemplificado con la variable (peso_usuario) que 
+para esta version seran 77 kilogramos*/
+var peso_usuario = 77;
+
+//operacion matematica que dira el precio del usuario en marte
+peso_final = peso_usuario * g_marte / g_tierra;
+
+//Retorno en el document: esta linea escribe algo en el documento html donde se encuentra vinculado
+document.write("tu peso en marte es: <b>"+peso_final+" kg </b>")
